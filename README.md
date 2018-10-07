8ae04dc19a94845d7096833437b201a411dd40e0
chris408 <chris408@users.noreply.github.com>
2018-10-07 11:57:20 -0700

    Added exception handling.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 This is a simple python script to collect sub domains from the Virus Total API. 
 
-First setup an environment variable `VTAPIKEY` with your Virus Total API key.
+First setup an environment variable `VTAPIKEY` with your [Virus Total](https://www.virustotal.com) API key. 
 
-`export VTAPIKEY=<apikey>`
-
+```shell
+unset HISTFILE #to avoid logging your key to ~/.bash_history
+export VTAPIKEY=<apikey>
+```
 Then do a search:
 
 Example: `python3 vt-subdomains.py apple.com`
--- vt-subdomains.py
+++ vt-subdomains.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import requests
 from os import environ
 import os
@@ -7,21 +8,28 @@ import json
 def main(domain, apikey):
     url = 'https://www.virustotal.com/vtapi/v2/domain/report'
     params = {'apikey':apikey,'domain':domain}
-    response = requests.get(url, params=params)
-    jdata = response.json()
-    domains = sorted(jdata['subdomains'])
+    try:
+        response = requests.get(url, params=params)
+        jdata = response.json()
+        domains = sorted(jdata['subdomains'])
+    except(KeyError):
+        print("No domains found for %s" % domain)
+        exit(0)
+    except(requests.ConnectionError):
+        print("Could not connect to www.virtustotal.com", file=sys.stderr)
+        exit(1)
 
     for domain in domains:
         print(domain)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-    	print("Usage: python3 vt-subdomains.py domain.com")
+    	print("Usage: python3 vt-subdomains.py domain.com", file=sys.stderr)
     	sys.exit(1) 
     domain = sys.argv[1]
     if environ.get('VTAPIKEY'):
         apikey = os.environ['VTAPIKEY']
     else:
-        print("VTAPIKEY environment variable not set. Quitting.")
+        print("VTAPIKEY environment variable not set. Quitting.", file=sys.stderr)
         sys.exit(1)
     main(domain, apikey)
